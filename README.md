1f0b1b90694c0060bc2ca0ab5e338fb123abffb9
DonnieDice <DonnieDice@ProtonMail.com>
2022-03-18 02:17:12 -0500

    v1.0.2d
    Added - CHANGELOG.txt

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-FFLU
+v1.0.2d
+ ------------------------------
+
+FFLU - Final Fantasy Level Up!
+
+ ------------------------------
+
+This is an addon that will play the "LEVEL UP!" chime from Final Fantasy when you level up in WoW.
+
+ ------------------------------
+
+This addon works in SHADOWLANDS!
+
+This addon works in CLASSIC WOW!
+
+This addon works in CLASSIC TBC!
---
+++ main.lua
@@ -0,0 +1,16 @@
+--v1.0.2d
+-- Event function level up
+local a = CreateFrame("Frame")
+a:RegisterEvent("PLAYER_LEVEL_UP")
+a:SetScript("OnEvent",
+	function(self, event, ...)
+		PlaySoundFile("Interface\\Addons\\FFLU\\FFLU.ogg", "Master")
+	end)
+
+-- Mute default level up sound
+local b = CreateFrame("Frame")
+b:RegisterEvent("ADDON_LOADED")
+b:SetScript("OnEvent",
+	function(self, event, ...)
+		MuteSoundFile(569593)
+	end)
