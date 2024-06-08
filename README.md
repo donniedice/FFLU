0b15483a0fd0c18fbfa40a27c05842e07a07a838
DonnieDice <DonnieDice@ProtonMail.com>
2024-06-08 07:21:41 -0500

    v2.0.3
    - Removed - Notation - [core.lua]
    - Updated - github/workflow - [release.yml]
    - Removed - Version # - [.pkgmeta]
    - Removed - Version # - [README.md]
    - Removed - Version # - [CHANGES.md]

--- README.md
+++ README.md
@@ -1,10 +1,6 @@
 [![](https://img.shields.io/static/v1?label=Donate&message=CashApp&color=brightgreen)](https://bit.ly/3fyxxSU)
 
-v2.0.1
-------------------------------
-
 **FFLU | Final Fantasy Level Up!**
-
 ------------------------------
 
 ***Functionality has been merged into [BLU | Better Level Up!](https://www.curseforge.com/wow/addons/blu-better-level-up "This link takes you to the Curseforge.com website, you may download it here and help support the developers."). If you're a fan of my sound addons, [BLU | Better Level Up!](https://www.curseforge.com/wow/addons/blu-better-level-up "This link takes you to the Curseforge.com website, you may download it here and help support the developers.") is available from all addon provider websites. Thank you!***
--- data/core.lua
+++ data/core.lua
@@ -1,23 +1,13 @@
---v2.0.1
-
--- Level up sound effect
--- This function plays a sound effect when the player levels up
 local frame_a = CreateFrame("Frame")
 frame_a:RegisterEvent("PLAYER_LEVEL_UP")
 frame_a:SetScript("OnEvent", function(self, event, ...)
 	PlaySoundFile("Interface\\Addons\\FFLU\\Sounds\\FFLU.ogg", "Master")
 end)
-
--- Mute default level up sound
--- This function mutes the default level up sound
 local frame_b = CreateFrame("Frame")
 frame_b:RegisterEvent("ADDON_LOADED")
 frame_b:SetScript("OnEvent", function(self, event, ...)
 	MuteSoundFile(569593)
 end)
-
--- Chat Message
--- This function displays a chat message when the player logs in, informing them that the addon will no longer receive updates and has been merged into another addon
 local frame_c = CreateFrame("Frame")
 frame_c:RegisterEvent("PLAYER_LOGIN")
 frame_c:SetScript("OnEvent", function(self, event, text, ...)
